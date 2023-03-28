88d1adbb0a236b141014c2b67917af4ecd35a34e
vitouz <vitorsachi86@gmail.com>
2023-03-28 20:55:17 -0300

    autoplay and new navbar

--- despachante_with_sliderImage/JS/nav.js
+++ despachante_with_sliderImage/JS/nav.js
@@ -1,20 +1,11 @@
 const navSlide = () => {
     const burguer = document.querySelector('.burger')
-    const nav = document.querySelector('.menu')
-    const navLinks = document.querySelectorAll('.menu li')
+    const nav = document.querySelector('.navigation')
+    const navLinks = document.querySelectorAll('.navigation-items a')
 
     burguer.addEventListener('click', () => {
          // Toggle nav
-        nav.classList.toggle('nav-active')
-
-        //Animate Links
-        navLinks.forEach((link, index) => {
-            if(link.style.animation){
-                link.style.animation = '';
-            } else {
-                link.style.animation = `navLinkFade 0.5s ease forwards ${index / 7 + 0.5}s`
-            }
-        })
+        nav.classList.toggle('active')
         // Burguer animation
 
         burguer.classList.toggle('toggle');
